7a5521088da6f76cb12e328554895315f6ab2d6d
Jerzy Możdżer <s18351@pjwstk.edu.pl>
2022-12-19 21:41:06 +0100

    create project

---
+++ ConsoleAppCSV/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleAppCSV
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
