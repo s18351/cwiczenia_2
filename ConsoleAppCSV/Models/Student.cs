cbc6c557bfb2ed94ba3c45dc3dd1cc5ee5f47d85
Jerzy Możdżer <s18351@pjwstk.edu.pl>
2022-12-20 12:27:45 +0100

    start

---
+++ ConsoleAppCSV/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppCSV.Models
+{
+    public class Student
+    {
+        //Paweł,Kowalski375,Informatyka dzienne,Dzienne,4533,2000-02-12,375@pjwstk.edu.pl,Alina,Adam
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int IndexNumber{ get; set; }
+        public string Birthdate { get; set; }
+        public string Email { get; set; }
+        public string MotherName { get; set; }
+        public string FatherName { get; set; }
+
+    }
+}
---
+++ ConsoleAppCSV/Models/Studies.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppCSV.Models
+{
+    public class Studies
+    {
+        public string Name { get; set; }
+        public string Mode { get; set; }
+
+        public Student[] Students { get; set; }
+    }
+}
